a578daf3671ff861316728c3dc7fd9a9b8719312
TheZInc <154614743+TheZincTZ@users.noreply.github.com>
2025-05-15 13:20:31 +0800

    update packages
    v1 unreleased

--- chatbot.py
+++ chatbot.py
@@ -1,5 +1,5 @@
-from langchain.chat_models import ChatOpenAI
-from langchain.schema import HumanMessage, SystemMessage
+from langchain_community.chat_models import ChatOpenAI
+from langchain_core.messages import HumanMessage, SystemMessage
 from langchain.memory import ConversationBufferMemory
 from langchain.chains import ConversationChain
 import config
